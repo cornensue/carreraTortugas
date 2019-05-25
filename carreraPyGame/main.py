2f1d5d892d77436bc4cff99829cd2776d49290b2
Cornelio Nsue <mastarichy8@gmail.com>
2019-05-25 02:34:05 +0200

    Añadiendo funcionalidad con el metodo competir

--- carreraPyGame/main.py
+++ carreraPyGame/main.py
@@ -9,12 +9,41 @@ class Game():
    corredores = []
 
    def __init__(self):
-      self.__screen = pygame.display.set_mode(640, 480) #Crear la pantalla y fijar su tamaño
+      self.__screen = pygame.display.set_mode((640, 480)) #Crear la pantalla y fijar su tamaño
       pygame.display.set_caption("Carrera de bichos")
       self.background = pygame.image.load("images/background.png")
+
+      self.runner = pygame.image.load("images/smallball.png")
+   
+   def competir(self):
+
+      x = 0
+      hayGanador = False
+
+      while True:
+         # Coprobacion de los eventos
+         for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+               pygame.quit()
+               sys.exit()
+
+         # Refrescar / renderizar la pantalla
+         self.__screen.blit(self.background, (0,0))
+         self.__screen.blit(self.runner, (x, 240))
+         pygame.display.flip()
+         x += 3
+         if x >= 250:
+            hayGanador = True
+         
+      pygame.quit()
+      sys.exit()
+
 
 if __name__ == "__main__":
    pygame.init()
    game = Game()
+   game.competir()
+
+
 
 print(game)
